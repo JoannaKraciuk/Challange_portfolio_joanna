c8047cce750d2bb566817aa3ff96dbfa609bc8e3
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-07-06 11:38:55 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,7 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath = "//span"
+    sign_in_button_xpath = "//span[1]"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
