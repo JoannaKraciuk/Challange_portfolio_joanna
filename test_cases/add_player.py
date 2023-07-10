233c6c7590ef3b5b999932ce78026b48ac66800b
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-07-10 18:31:10 +0200

    commit add player

---
+++ test_cases/add_player.py
@@ -0,0 +1,32 @@
+import os
+import time
+import unittest
+from selenium import webdriver
+from pages.dashboard_page import Dashboard
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestAddPlayerPage(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl/')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_login_page(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('user10@getnada.com')
+        user_login.enter_password('Test-1234')
+        user_login.sign_in()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.add_player_page()
+        time.sleep(5)
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
