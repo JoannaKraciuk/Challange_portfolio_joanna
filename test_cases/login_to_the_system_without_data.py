ac6830d91e9d768a6bf18234ab67c827c52efbc0
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-07-19 09:12:59 +0200

    commit login to the system without data

---
+++ test_cases/login_to_the_system_without_data.py
@@ -0,0 +1,31 @@
+import os
+import time
+import unittest
+from selenium import webdriver
+
+from pages.base_page import BasePage
+from pages.dashboard_page import Dashboard
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_login_page_without_data(self):
+        user_login = LoginPage(self.driver)
+        user_login.title_of_page()
+        user_login.type_in_email('')
+        user_login.enter_password('')
+        user_login.sign_in()
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
