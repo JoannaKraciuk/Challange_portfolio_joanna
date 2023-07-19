06e0c2d3721cccd327617ae08bdf5c12a697c089
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-07-19 09:11:57 +0200

    commit change language on dashboard page

---
+++ test_cases/change_language_on_dashboard_page.py
@@ -0,0 +1,34 @@
+import os
+import time
+import unittest
+from selenium import webdriver
+
+from pages.base_page import BasePage
+from pages.dashboard_page import Dashboard
+from pages.login_page import LoginPage
+from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
+
+
+class TestLoginPage(unittest.TestCase):
+
+    @classmethod
+    def setUp(self):
+        os.chmod(DRIVER_PATH, 755)
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver.get('https://scouts-test.futbolkolektyw.pl')
+        self.driver.fullscreen_window()
+        self.driver.implicitly_wait(IMPLICITLY_WAIT)
+
+    def test_login_page(self):
+        user_login = LoginPage(self.driver)
+        element_text = BasePage(self.driver)
+        element_text.assert_element_text(self.driver, '//div/h5', 'Scouts Panel')
+        user_login.type_in_email('user10@getnada.com')
+        user_login.enter_password('Test-1234')
+        user_login.sign_in()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.change_language()
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
