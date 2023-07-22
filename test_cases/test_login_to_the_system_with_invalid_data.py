2f6750854037344a30cecf6bea6017f682ff9f86
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-07-22 19:52:12 +0200

    commit test login to the system with invalid data

--- test_cases/test_login_to_the_system_with_invalid_data.py
+++ test_cases/test_login_to_the_system_with_invalid_data.py
@@ -2,14 +2,14 @@ import os
 import time
 import unittest
 from selenium import webdriver
-
+import pytest
 from pages.base_page import BasePage
 from pages.dashboard_page import Dashboard
 from pages.login_page import LoginPage
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 
 
-class TestLoginPage(unittest.TestCase):
+class TestLoginPageInvalidData(unittest.TestCase):
 
     @classmethod
     def setUp(self):
